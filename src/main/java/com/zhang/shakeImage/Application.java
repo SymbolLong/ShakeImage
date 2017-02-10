5413262b89c4101a3910f0fa9e2894cf6f2f3b98
SymbolLong <877857078@qq.com>
2017-02-10 15:16:18 +0800

    链接mysql
    项目初始化

---
+++ src/main/java/com/zhang/shakeImage/Application.java
@@ -0,0 +1,15 @@
+package com.zhang.shakeImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhangsl on 2017/2/10.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/com/zhang/shakeImage/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.zhang.shakeImage.repository;
+
+import com.zhang.shakeImage.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by zhangsl on 2017/2/10.
+ */
+public interface UserRepository  extends CrudRepository<User,Long>{
+
+
+}

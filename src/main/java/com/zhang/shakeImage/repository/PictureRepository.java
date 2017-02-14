80b3c30ff85c38babdf0951fa3060415ae191da7
zhangshenglong <zhangshenglong@yscredit.com>
2017-02-14 13:02:42 +0800

    修改方法

--- src/main/java/com/zhang/shakeImage/repository/PictureRepository.java
+++ src/main/java/com/zhang/shakeImage/repository/PictureRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.zhang.shakeImage.entity.Picture;
 
@@ -15,7 +16,7 @@ public interface PictureRepository extends CrudRepository<Picture, Long> {
 
 	Picture findByMd5(String md5);
 
-	@Query("select new Picture(p.id) from Picture p where contentType = 'text/html' ")
-	List<Picture> findByContentType(Pageable page);
+	@Query("select new Picture(p.id,p.url,p.md5,p.type,p.statusCode,p.contentType,p.createTime,p.updateTime) from Picture p where contentType = :type ")
+	List<Picture> findByContentType(@Param("type")String type,Pageable page);
 
 }
